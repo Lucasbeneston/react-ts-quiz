b5dafa97bfbf11a86fd4ad63d63e51ea5885bc50
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2021-03-16 14:21:55 +0100

    :rocket: Add quiz logic :rocket:

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// Components
+import QuestionsQuiz from "./components/pages/QuestionsQuizView";
+
+const App = () => {
+  return (
+    <div className="App">
+      <QuestionsQuiz />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/organisms/QuestionCard.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+// Types
+import { AnswerObject } from "../pages/QuestionsQuizView";
+
+type Props = {
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
+  questionNumber: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNumber,
+  totalQuestions,
+}) => {
+  return (
+    <div>
+      <p>
+        Question: {questionNumber} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <div>
+        {answers.map((answer) => (
+          <div key={answer}>
+            <button disabled={!!userAnswer} value={answer} onClick={callback}>
+              <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
